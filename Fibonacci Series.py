30d8ab03df2506dbca7c4ef4ecf3565bd7bb7ffe
gagank49 <gagank49@gmail.com>
2022-12-13 15:56:13 +0530

    Update Fibonacci Series.py

--- Fibonacci Series.py
+++ Fibonacci Series.py
@@ -1,19 +1,3 @@
 
-v=10
-l=[]
-for i in range(v):
-    if(i==0):
-        l.append(0)
-    elif(i==1):
-        l.append(1)
-    else:
-        l.append(l[i-1]+l[i-2])
-print(l)
-
-def fib(n):
-    if(n==0):
-        return 0
-    elif(n==1):
-        return 1
-    fib(n-1)
-    
+for i in range(10):
+    print('hello')
